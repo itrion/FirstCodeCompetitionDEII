13c8eeeac0df125b193e51bcd147a3ef6b524a56
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-05-05 22:18:21 +0100

    Defining the horizontal search

---
+++ TheLostWords/test/HorizontalSearchTest.java
@@ -0,0 +1,51 @@
+import org.challenge.Coordinate;
+import org.challenge.NounFinder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HorizontalSearchTest {
+    @Test
+    public void theNounIsInTheFirstRow() {
+        char[][] input = {
+                {'a', 'b', 'c', 'd', 'a', 'a', 'b'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'}};
+        String noun = "aab";
+        NounFinder nounFinder = new NounFinder(input);
+        assertEquals(new Coordinate(0, 4), nounFinder.doAHorizontalSearch(noun).nounBegin());
+        assertEquals(new Coordinate(0, 6), nounFinder.doAHorizontalSearch(noun).nounEnd());
+    }
+
+    @Test
+    public void theNounIsInTheSecondRow() {
+        char[][] input = {
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'a', 'b', 'c', 'a', 'a', 'b', 'b'}};
+        String noun = "aab";
+        NounFinder nounFinder = new NounFinder(input);
+        assertEquals(new Coordinate(1, 3), nounFinder.doAHorizontalSearch(noun).nounBegin());
+        assertEquals(new Coordinate(1, 5), nounFinder.doAHorizontalSearch(noun).nounBegin());
+    }
+
+    @Test
+    public void theNounIsReversedInTheFirsRow() {
+        char[][] input = {
+                {'a', 'b', 'c', 'a', 'a', 'b', 'b'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'}};
+        String noun = "acb";
+        NounFinder nounFinder = new NounFinder(input);
+        assertEquals(new Coordinate(0, 3), nounFinder.doAHorizontalSearch(noun).nounBegin());
+        assertEquals(new Coordinate(0, 1), nounFinder.doAHorizontalSearch(noun).nounBegin());
+    }
+
+    @Test
+    public void theNounIsReversedInTheSecondRow() {
+        char[][] input = {
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'a', 'b', 'c', 'a', 'a', 'b', 'b'}};
+        String noun = "baacba";
+        NounFinder nounFinder = new NounFinder(input);
+        assertEquals(new Coordinate(1, 5), nounFinder.doAHorizontalSearch(noun).nounBegin());
+        assertEquals(new Coordinate(1, 0), nounFinder.doAHorizontalSearch(noun).nounBegin());
+    }
+}
