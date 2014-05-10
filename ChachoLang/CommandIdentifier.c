06f26875a8d0b638d4307decdc43fbe37bb8de5a
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-05-10 17:55:08 +0100

    code cleaned

--- ChachoLang/CommandIdentifier.c
+++ ChachoLang/CommandIdentifier.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 
+const int FIVE_LETTERS_INDEX = 0;
+const int SIX_LETTERS_INDEX = 2;
+const int SEVEN_LETTERS_INDEX = 6;
+const int EIGHT_LETTERS_INDEX = 14;
+const int NINE_LETTERS_INDEX = 15;
+const int THIRTEEN_LETTERS_INDEX = 16;
+
 const char* commandsNames[] = {
 	"quita\0",
 	"vetea\0",
@@ -21,35 +28,18 @@ const char* commandsNames[] = {
 	"intentalo\0",
 	"voyarepetirme\0"};
 
-int identify(char* commandName);
-char* cleanCommand(char* command);
-char* removeSemicolon(char* command);
-
-int identifyCommand(char* commandText){
-	return identify(cleanCommand(commandText));
-}
-
-char* cleanCommand(char* command){
-	return removeSemicolon(command);
-}
-
-char* removeSemicolon(char* command){
-	command[strlen(command) - 1] = '\0';
-	return command;
-}
-
-int identify(char* commandName){
-	for (int i = 0; i <= 1; ++i){
+int identifyCommand(char* commandName){
+	for (int i = FIVE_LETTERS_INDEX; i < SIX_LETTERS_INDEX; ++i){
 		if(strncmp(commandName, commandsNames[i], 5) == 0) return i;
 	}
-	for (int i = 2; i <=5; ++i){
+	for (int i = SIX_LETTERS_INDEX; i < SEVEN_LETTERS_INDEX; ++i){
 		if(strncmp(commandName, commandsNames[i], 6) == 0) return i;
 	}
-	for (int i = 6; i <=13; ++i){
+	for (int i = SEVEN_LETTERS_INDEX; i < EIGHT_LETTERS_INDEX; ++i){
 		if(strncmp(commandName, commandsNames[i], 7) == 0) return i;
 	}
-	if(strncmp(commandName, commandsNames[14], 8) == 0) return 14;
-	if(strncmp(commandName, commandsNames[15], 9) == 0) return 15;
-	if(strncmp(commandName, commandsNames[16], 13) == 0) return 16;
+	if(strncmp(commandName, commandsNames[EIGHT_LETTERS_INDEX], 8) == 0) return 14;
+	if(strncmp(commandName, commandsNames[NINE_LETTERS_INDEX], 9) == 0) return 15;
+	if(strncmp(commandName, commandsNames[THIRTEEN_LETTERS_INDEX], 13) == 0) return 16;
 	return -1;
 }
