e9ee7a5a116a511af7a33f59c687e05ba48559a3
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-05-12 10:07:10 +0100

    renamed command which compiles and run the tests

--- ChachoLang/runTests.sh
+++ ChachoLang/runTests.sh
@@ -9,11 +9,12 @@ fi;
 
 gcc -o AllTests \
 	CUtest/CuTest.c \
+	CommandIdentifier.c \
+	ChachoLangInterpreter.c \
+	Commands.c \
 	test/AllTest.c \
 	test/ChachoLangInterpreterTest.c \
 	test/CommandIdentifierTest.c \
-	test/CommandsBehaviourTest.c \
-	ChachoLangInterpreter.c \
-	CommandIdentifier.c
+	test/CommandsBehaviourTest.c
 
 time ./AllTests
